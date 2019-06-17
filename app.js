906ffb16a33afa0bc1edfa90b56f764a8b7524dc
Mackenzie Weaver <macknz7@gmail.com>
2019-06-17 17:49:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-/*                              variables                              */
-
-let play = true;
+/*variables*/
 // up-down length adjust buttons
 const breakDecremment = document.getElementById("break-decrement");
 const sessionDecremment = document.getElementById("session-decrement");
@@ -8,25 +6,20 @@ const breakIncremment = document.getElementById("break-increment");
 const sessionIncremment = document.getElementById("session-increment");
 const breakLength = document.getElementById("break-length");
 const sessionLength = document.getElementById("session-length");
-// reset button
-const reset = document.getElementById("reset");
-// timer
-const timer = document.getElementById("time-left");
-// play/pause button
-const startStop = document.getElementById("start_stop");
+const reset = document.getElementById("reset"); // reset button
+const timer = document.getElementById("time-left"); // timer
+const startStop = document.getElementById("start_stop"); // play/pause button
 
-/*                               listeners                             */
+/*listeners*/
 // up-down length adjust listeners
 breakDecremment.addEventListener("click", decrementBreak);
 sessionDecremment.addEventListener("click", decrementSession);
 breakIncremment.addEventListener("click", incrementBreak);
 sessionIncremment.addEventListener("click", incrementSession);
-// reset button
-reset.addEventListener("click", resetIt);
-// play/pause button
-startStop.addEventListener("click", playOrPause);
+reset.addEventListener("click", resetIt); // reset button
+startStop.addEventListener("click", playbutton); // play/pause button
 
-/*                               functions                             */
+/*functions*/
 // up-down length adjust functions
 function decrementBreak() {
   let value = parseInt(breakLength.innerHTML);
@@ -37,6 +30,7 @@ function decrementSession() {
   let value = parseInt(sessionLength.innerHTML);
   if (value >= 0) { sessionLength.innerHTML = value - 1; }
   timer.innerHTML = sessionLength.innerHTML + ':' + '00';
+  stopTimer();
 }
 
 function incrementBreak() {
@@ -48,6 +42,7 @@ function incrementSession() {
   let value = parseInt(sessionLength.innerHTML);
   if (value < 60) { sessionLength.innerHTML = value + 1; }
   timer.innerHTML = sessionLength.innerHTML + ':' + '00';
+  stopTimer();
 }
 
 // reset button
@@ -57,22 +52,39 @@ function resetIt() {
   timer.innerHTML = '25:00';
 }
 
-// play/pause button
-function playOrPause() {
-    let time = timer.innerHTML.split('');
-    let minutes = parseInt(time[0] + time[1]);
-    let seconds = parseInt(time[3] + time[4]);
-    // total seconds
-    let duration = minutes * 60 + seconds; 
-    let sessionTimer = setInterval (timeIt, 1000);
-    function timeIt() {
-      duration = duration - 1;
-      timer.innerHTML = 
-      Math.floor(duration / 60) + 
-      ':' + 
-      Math.floor(duration % 60);
-      if (duration === 0) {
-        clearInterval(sessionTimer);
-      }
-    }
+function getTime() {
+  let time = timer.innerHTML.split('');
+  let minutes = parseInt(time[0] + time[1]);
+  let seconds = parseInt(time[3] + time[4]);
+  return minutes * 60 + seconds; // total seconds
+}
+
+function displayTime() {
+  let duration = getTime();
+  if (--duration === 0) { return; }
+  timer.innerHTML =
+    Math.floor(duration / 60) + ':' +
+    Math.floor(duration % 60);
+}
+
+let counter = 0;
+let timerID = 0;
+function playbutton() {
+  if (++counter % 2 !== 0) {
+    timerID = startTimer();
+    startStop.firstChild.classList.remove('fa-play');
+    startStop.firstChild.classList.add('fa-pause');
+  } else {
+    stopTimer();
+    startStop.firstChild.classList.add('fa-play');
+    startStop.firstChild.classList.remove('fa-pause');
+  }
+}
+
+let startTimer = function () {
+  return setInterval(displayTime, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerID);
 }
